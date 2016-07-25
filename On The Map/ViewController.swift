0454ecd0dab10a7f3991c8fae8258b7fda92e855
Jess Gates <jessgates@jess-gatess-macbook-pro.local>
2016-07-25 09:09:08 -0500

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Jess Gates on 7/25/16.
+//  Copyright © 2016 Jess Gates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
